7f25f53d076ebb81bd1f682c8235a3f09eefd743
Keon Jukes <keonofthephanerozoiceon@gmail.com>
2023-12-25 13:32:24 -0800

    barebones portfolio

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+
+</script>
+
+<template>
+  <header>
+    <h1>Keon Jukes</h1>
+  </header>
+
+  <main>
+    <p>Welcome to my laboratory</p>
+  </main>
+</template>
+
+<style scoped>
+
+</style>
