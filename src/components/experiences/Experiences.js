ff867cd9f7187f5a8c8ec4d56730537f7a6bd0d1
Keon Jukes <keonofthephanerozoiceon@gmail.com>
2023-12-25 15:23:44 -0800

    adding an experience to my portfolio

--- src/components/experiences/Experiences.js
+++ src/components/experiences/Experiences.js
@@ -2,12 +2,21 @@ export const experiences = [
   {
     title: 'Associate Software Engineer',
     company: 'Dolby.io',
-    startDate: 'September 26, 2022',
+    startDate: 'September 2022',
     endDate: null,
     description: 
     'Developing high quality user interfaces for both internal and customer facing projects.\
      I currently am working on Dolby.io\'s Real-Time Streaming Dashboard to deliver a intuitive\
      experience for customers.',
      technologies: ['React', 'TypeScript', 'Vue', 'JavaScript', 'SCSS', 'Material UI', 'Bootstrap', 'Keycloak']
+  },
+  {
+    title: 'Sales Representative',
+    company: 'Facebook (Meta)',
+    startDate: 'September 2020',
+    endDate: 'January 2021',
+    description: 
+    'Sold and conducted demos for the Oculus Quest VR headsets in retail stores.\
+     Exceeded monthly and weekly sales goals to meet Holiday goals.'
   }
 ]
