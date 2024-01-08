9a2d92c5d5f4dbc06136c73821258bffd6d7178a
Keon Jukes <keonofthephanerozoiceon@gmail.com>
2024-01-07 16:18:19 -0800

    Portfolio copy update

--- src/components/BiographySection.vue
+++ src/components/BiographySection.vue
@@ -5,7 +5,7 @@
     <h2 class="biography-section">About</h2>
     <br />
     <p class="biography-text">
-      During 2020's suprise pandemic I decided to make a transition from sales to software
+      During 2020's suprise pandemic, I decided to make a transition from tech sales to software
       engineering. After completing Fullstack Academy's software engineering immersive bootcamp, I
       joined Dolby.io as an Associate Software Engineer and have since been working on various
       projects to deliver great user interfaces (UI) and user experiences (UX).
